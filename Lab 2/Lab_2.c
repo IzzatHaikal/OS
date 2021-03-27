fe7e773474222442a85dd53f40c1e19ccfb311a3
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2021-03-27 19:56:28 +0800

    FIFO and LRU simulation using C

--- Lab 2/Lab_2.c
+++ Lab 2/Lab_2.c
@@ -3,7 +3,9 @@
 
 void FIFO(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageNum, int pageOrder[]);
 void switchFIFO(int pageFrameNum, int pageFrame[], int pageNum, int pageOrder[]);
-// void LRU(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageNum, int pageOrder[])
+void LRU(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageNum, int pageOrder[]);
+bool findLRU(int pageFrameNum, int pageFrame[], int pageOrder[], int find);
+int indexLRU(int pageFrameNum, int pageFrame[], int pageOrder[], int find);
 
 int main(){
     int pageFrameNum;
@@ -24,10 +26,10 @@ int main(){
     scanf("%d", &choice);
     if(choice == 1)
         FIFO(pageFrameNum, pageFrame, pageFrameAllocate, pageNum, pageOrder);
-    // else if (choice == 2)
-        // LRU(pageFrameNum, pageFrame, pageFrameAllocate, pageNum, pageOrder);
-    // else
-    //     printf("Incorrect Input");
+    else if (choice == 2)
+        LRU(pageFrameNum, pageFrame, pageFrameAllocate, pageNum, pageOrder);
+    else
+        printf("Incorrect Input");
     
 }
 
@@ -59,7 +61,7 @@ void FIFO(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageN
         if(allocate == true)
             missCount++;
     }
-    printf("%d", missCount);
+    printf("Total Page Fault = %d", missCount);
 }
 
 void switchFIFO(int pageFrameNum, int pageFrame[], int pageNum, int pageOrder[]){
@@ -73,6 +75,64 @@ void switchFIFO(int pageFrameNum, int pageFrame[], int pageNum, int pageOrder[])
     }
 }
 
-// void LRU(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageNum, int pageOrder[]){
+void LRU(int pageFrameNum, int pageFrame[], bool pageFrameAllocate[], int pageNum, int pageOrder[]){
+    int pageFrameBit[pageFrameNum];
+    int missCount = 0;
+    bool firstEntry = false;
+    for(int i=0; i<pageNum; i++){
+        bool allocate = false;
+        int smallest = -1;
+        for(int j=0; j<pageFrameNum; j++){
+            if(pageFrameAllocate[j] == false && firstEntry == false){
+                pageFrameAllocate[j] = true;
+                pageFrame[j] = pageOrder[i];
+                pageFrameBit[j] = i;
+                allocate = true;
+                if (i == pageFrameNum-1)
+                    firstEntry = true;
+                break;
+            }
+            else if(pageFrameAllocate[j] == true && firstEntry == true){
+                if(smallest == -1)
+                    smallest = j;
+                else if(pageFrameBit[smallest] > pageFrameBit[j])
+                    smallest = j;
+            }
+        }
+        if (smallest != -1){
+            if (findLRU(pageFrameNum, pageFrame, pageOrder, i)){
+                pageFrameBit[indexLRU(pageFrameNum,pageFrame,pageOrder,i)] = i;
+            }
+            else{
+                pageFrame[smallest] = pageOrder[i];
+                pageFrameBit[smallest] = i;
+                allocate = true;
+            }
+        }
+        if(allocate == true)
+            missCount++;
+    }
+    printf("Total Page Fault = %d", missCount);
+}
 
-// }
+bool findLRU(int pageFrameNum, int pageFrame[], int pageOrder[], int find){
+    bool found = false;
+    for(int i=0; i<pageFrameNum; i++){
+        if(pageFrame[i] == pageOrder[find]){
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+int indexLRU(int pageFrameNum, int pageFrame[], int pageOrder[], int find){
+    int index;
+    for(int i=0; i<pageFrameNum; i++){
+        if(pageFrame[i] == pageOrder[find]){
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
